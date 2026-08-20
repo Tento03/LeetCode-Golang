e261e33b1f41c0d7c242f18fb5fe3b049db25432
Tento <regartento@gmail.com>
2026-08-20 16:37:33 +0700

    implement containsDuplicate function to check for duplicate integers in a slice

---
+++ 217.ContainsDuplicate.go
@@ -0,0 +1,15 @@
+package main
+
+func containsDuplicate(nums []int) bool {
+	seen := map[int]bool{}
+
+	for _, num := range nums {
+		if seen[num] {
+			return true
+		}
+
+		seen[num] = true
+	}
+
+	return false
+}
