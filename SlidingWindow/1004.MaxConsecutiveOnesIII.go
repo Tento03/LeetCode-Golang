0237472bd3ce4d0ea7247f93dda6041b9a14bee7
Tento <regartento@gmail.com>
2026-09-05 09:50:05 +0700

    implement longestOnes function to find the longest subarray with at most k zeros

---
+++ SlidingWindow/1004.MaxConsecutiveOnesIII.go
@@ -0,0 +1,29 @@
+package main
+
+func longestOnes(nums []int, k int) int {
+	left := 0
+	zeros := 0
+	maxLen := 0
+
+	for right := 0; right < len(nums); right++ {
+		if nums[right] == 0 {
+			zeros++
+		}
+
+		for zeros > k {
+			if nums[left] == 0 {
+				zeros--
+			}
+
+			left++
+		}
+
+		windowLen := right - left + 1
+
+		if windowLen > maxLen {
+			maxLen = windowLen
+		}
+	}
+
+	return maxLen
+}
